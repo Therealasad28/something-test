42c3b69b4cd05f50d77a6e471a6ec9dc61a4dfcc
TheRealAsad28 <therealasad28@gmail.com>
2022-12-12 23:03:20 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+"use strict";
+
+// Date TYPES
+
+// let number = 28; // Number
+// console.log(8 / 0); // Infinity (Cheksizlik)
+// console.log(-8 / 0); // -Infinity (Cheksizlik)
+// console.log("Asad" * 8); // NaN (Note a number) {mumkin emas}
+
+// const clientName = `Asadbek`; // String
+
+// // Boolean true/false
+// const uylanganmsan = false;
+// const isCircleworld = true;
+
+// // null
+// console.log();  // null bu yoq ozgaruvchini chaqirish.
+
+// // undefined
+// let something;
+// console.log(something); // undefined bu ozgaruvchi bor lekn qiymati yoq.
+
+// // Object
+// const thief = {
+// // key: value ;
+//     age: 18,  // number
+//     isLong: true,  // Boolean
+//     hair: "Black",   // String
+// };
+// console.log(thief.hair);
+
+// // array
+// const cars = ["Gentra", "Malibu", "Matiz", "Damas", "Cobalt", "Tracker"];
+// console.log(cars[0]);
+
+// const place = confirm("Are you here");
+// const currentPlace = prompt("Where are you", "");
+// console.log(currentPlace);
+
+// const car = [];
+
+//  car[0] = prompt("What's you favourite car? #1", "");
+//  car[1] = prompt("What's you favourite car? #2", "");
+//  car[2] = prompt("What's you favourite car? #3", "");
+//  console.log(car);
+
+// const profil = prompt("what's your favourite Bloger?");
+// console.log("https://instagram.com/" + profil);
+
+// const user = "Asad";
+// const job = "Everbest IT Academy manager. ";
+
+// console.log(`User name is ${user}, he is ${job}`);
+
+// console.log(988 + +"2");
+
+// incr, decr
+
+// let incr = 28;
+// let decr = 28;
+
+// console.log(incr++); // (++) +1 Qo'shadi
+// console.log(decr--); // (--) -1 Ayiradi
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(incr);
+// console.log(decr);
+
+// console.log(50 % 4); // (%) Sonlarni bo'ladi va qoldiqini ko'rsatadi.
+
+// console.log(8 * 8 == "64");
+
+// && = (va) degani; || = (yoki) degani;
+
+const isAge = false;
+const isClose = false;
+const isChecked = true;
+
+console.log(isAge || isClose || isChecked);
